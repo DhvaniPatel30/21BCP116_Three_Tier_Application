b598922c5fbd40399dc4350cbaee8c7c193b4a1c
Dhvani Patel <dhvanipatel301003@gmail.com>
2024-04-23 22:20:53 +0530

    done

---
+++ frontend/src/component/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Notes } from "./Notes";
+
+function Home() {
+  return (
+    <>
+    <h1>Hello World</h1>
+      <Notes />
+    </>
+  );
+}
+
+export default Home;
